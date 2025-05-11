d173243ab753d6c3231ecfeda22623c5ebd81b2c
ealzen <ealzen0110@gmail.com>
2025-05-11 20:35:15 +0800

    docs(Contact): 修改联系页面中的措辞
    将“与我们联系”改为“与我联系”，以更准确地反映个人联系信息

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -2,7 +2,7 @@
   <div class="contact">
     <h1>联系我</h1>
     <p>这是联系我的页面</p>
-    <p>您可以通过以下方式与我们联系：</p>
+    <p>您可以通过以下方式与我联系：</p>
     <a href="https://github.com/ealzen">github主页</a>
   </div>
 </template>
